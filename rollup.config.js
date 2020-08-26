ab85210cf72025929249599c96f2db70c5bfa8e2
Kyle Essex <kylegavinessex@hotmail.com>
2020-08-26 18:27:40 +0100

    fix: re-add support for IE flex

--- rollup.config.js
+++ rollup.config.js
@@ -3,13 +3,16 @@ import typescript from 'rollup-plugin-typescript2';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import postcss from 'rollup-plugin-postcss';
+import flexbugs from 'postcss-flexbugs-fixes';
 import postcssPresetEnv from 'postcss-preset-env/index.js';
 import scss from 'rollup-plugin-scss';
 import dartSass from 'dart-sass';
 
 process.chdir(__dirname);
 
-export default {
+const defaultBrowsers = ['defaults', 'not ie > 0'];
+
+const defaultCfg = {
   input: './src/_resources/entry.ts',
   output: {
     file: './resources/main.min.js',
@@ -26,7 +29,7 @@ export default {
         extract: true,
         minimize: true,
         plugins: [
-          postcssPresetEnv({browsers: 'defaults'}),
+          postcssPresetEnv({browsers: defaultBrowsers}),
         ],
         sourceMap: true,
       }),
@@ -42,3 +45,48 @@ export default {
     })
   ],
 };
+
+
+const ieBrowsers = ['ie > 9', '> 0.02%', 'last 2 versions', 'Firefox ESR'];
+
+const ieConfig = {
+  input: './src/_resources/entry_ie.ts',
+  output: {
+    file: './resources/main.ie.min.js',
+    format: 'iife',
+    sourcemap: true,
+  },
+  plugins: [
+    //css
+    scss({
+      runtime: dartSass
+    }),
+    postcss(
+      {
+        extract: true,
+        minimize: true,
+        plugins: [
+          flexbugs(),
+          postcssPresetEnv({browsers: ieBrowsers, autoprefixer: {grid: 'no-autoplace'}}),
+        ],
+        sourceMap: true,
+      }),
+    //js
+    resolve({browser: true, preferBuiltins: false}),
+    //js
+    resolve({browser: true, preferBuiltins: false}),
+    commonjs(),
+    typescript({
+      compilerOptions: {
+        target: 'ES3',
+      }
+    }),
+    terser({
+      output: {
+        comments: false,
+      },
+    })
+  ],
+};
+
+export default [defaultCfg, ieConfig];
