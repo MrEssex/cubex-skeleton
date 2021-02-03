1e6dfe5342a386244952f04513bc9be53c2b0830
Kyle Essex <kylegavinessex@hotmail.com>
2021-02-03 12:04:37 +0000

    - use custom feather icon component

---
+++ src/Components/AbstractComponent.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace CubexBase\Application\Components;
+
+use CubexBase\Application\AbstractBase;
+
+/**
+ * Class AbstractComponent
+ * @package CubexBase\Application\Components
+ */
+abstract class AbstractComponent extends AbstractBase
+{
+
+}
--- src/Pages/HomePage/Ui/HomePage.php
+++ src/Pages/HomePage/Ui/HomePage.php
@@ -2,11 +2,13 @@
 
 namespace CubexBase\Application\Pages\HomePage\Ui;
 
+use CubexBase\Application\Components\FeatherIcons\FeatherIcon;
 use CubexBase\Application\Pages\AbstractPage;
 use Packaged\Glimpse\Core\HtmlTag;
 use Packaged\Glimpse\Tags\Div;
 use Packaged\Glimpse\Tags\LineBreak;
 use Packaged\Glimpse\Tags\Text\HeadingTwo;
+use Packaged\Glimpse\Tags\Text\Paragraph;
 use Throwable;
 
 /**
@@ -33,7 +35,10 @@ class HomePage extends AbstractPage
     return [
       Div::create($this->_('hello_world_b10a', 'Hello World')),
       LineBreak::create(),
-      LineBreak::create(),
+      Paragraph::create(
+        FeatherIcon::withContext($this, 'activity'),
+        'This is something'
+      ),
       HeadingTwo::create('User Ip Information'),
       json_encode($this->getContext()->ip()->getIpData(), JSON_THROW_ON_ERROR),
       LineBreak::create(),
