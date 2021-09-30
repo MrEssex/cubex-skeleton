b90be7bf774675f5ad9a3a74e26e84dafc850ec2
Kyle Essex <kylegavinessex@hotmail.com>
2021-09-30 23:33:06 +0100

    Cleanup

--- src/Routing/Router.php
+++ src/Routing/Router.php
@@ -2,13 +2,11 @@
 
 namespace CubexBase\Application\Routing;
 
-use CubexBase\Application\Application\Authentication\AuthenticationRouter;
 use CubexBase\Application\Controllers\HomeController;
 
 class Router extends AbstractRouter
 {
   protected array $_routes = [
-    '/$'    => HomeController::class,
-    '/auth' => AuthenticationRouter::class,
+    '/$' => HomeController::class,
   ];
 }
