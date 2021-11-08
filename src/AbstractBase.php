abebabf932ead5b05c35336800f28c31835a1926
Kyle Essex <kylegavinessex@hotmail.com>
2021-11-08 14:17:00 +0000

    Move some defaults to seperate repo

--- src/AbstractBase.php
+++ src/AbstractBase.php
@@ -2,44 +2,23 @@
 
 namespace CubexBase\Application;
 
-use Cubex\I18n\GetTranslatorTrait;
 use CubexBase\Application\Context\Context;
 use Exception;
+use MrEssex\CubexFramework\AbstractBase as AbstractBaseAlias;
 use Packaged\Context\Context as PackagedContext;
-use Packaged\Context\ContextAware;
-use Packaged\Context\ContextAwareTrait;
-use Packaged\Context\WithContext;
-use Packaged\Context\WithContextTrait;
-use Packaged\I18n\Translatable;
-use Packaged\I18n\TranslatableTrait;
-use Packaged\Ui\Html\TemplatedHtmlElement;
-use PackagedUi\BemComponent\BemComponent;
-use PackagedUi\BemComponent\BemComponentTrait;
 
-abstract class AbstractBase extends TemplatedHtmlElement
-  implements WithContext, ContextAware, BemComponent, Translatable
+abstract class AbstractBase extends AbstractBaseAlias
 {
-  use BemComponentTrait;
-  use ContextAwareTrait;
-  use WithContextTrait;
-  use TranslatableTrait;
-  use GetTranslatorTrait;
-
   /**
    * @throws Exception
    */
   public function getContext(): PackagedContext
   {
-    if($this->_context instanceof Context)
+    if(parent::getContext() instanceof Context)
     {
-      return $this->_context;
+      return parent::getContext();
     }
 
     throw new Exception('Invalid Context Passed through');
-  }
-
-  public function __construct()
-  {
-    $this->addClass($this->getBlockName());
   }
 }
