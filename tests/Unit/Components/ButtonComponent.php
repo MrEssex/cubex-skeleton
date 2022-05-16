45505091ce57242a0684e9892beb1c9c9c2acc32
Kyle Essex <kylegavinessex@hotmail.com>
2022-05-16 14:20:57 +0100

    Remove Pest Add PHPStan.neon

--- tests/Unit/Components/ButtonComponent.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use CubexBase\Application\Components\ButtonComponent;
-
-test('Button Component Renders', function () {
-  $button = new ButtonComponent('Example');
-  expect($button->render())->toBe('<button class="btn">Example</button>');
-});
