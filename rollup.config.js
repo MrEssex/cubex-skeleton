2c04a6fe9c67d598fdee401d6268fdb1bf2f2d96
Kyle Essex <kylegavinessex@hotmail.com>
2022-11-07 20:33:45 +0000

    Use flutter remove tailwind

--- rollup.config.js
+++ rollup.config.js
@@ -1,50 +1,18 @@
-import postcss from 'rollup-plugin-postcss';
-import resolve from '@rollup/plugin-node-resolve';
+import Flutter from 'flutter';
 import postcssComments from 'postcss-discard-comments';
 import autoprefixer from 'autoprefixer';
-import commonjs from '@rollup/plugin-commonjs';
-import typescript from '@rollup/plugin-typescript';
-import tailwind from 'tailwindcss';
-import {terser} from 'rollup-plugin-terser';
 
-// `yarn watch` -> `production` is false
-// `yarn build` -> `production` is true
-// eslint-disable-next-line no-undef
-const production = !process.env.ROLLUP_WATCH;
-
-const commonPlugins = [
-  resolve(), // tells Rollup how to find node_modules packages
-  commonjs(),
-  typescript({'sourceMap': !production}), // run typescript compiler
-  commonjs({
-    exclude:      'node_modules',
-    ignoreGlobal: true
-  }),
-  production && terser() // minify, but only in production
+const plugins = [
+  postcssComments({removeAll: true}),
+  autoprefixer()
 ];
 
-const postcssPlugins = [
-  autoprefixer(),
-  postcssComments({'removeAll': true}),
-  tailwind()
-];
+Flutter
+  .setInputPath('assets/entry.ts')
+  .setOutputPath('main.js', 'resources/main.min.js')
+  .typescript()
+  .resolve()
+  .commonJs()
+  .postCss({plugins: plugins});
 
-export default {
-  input:   'assets/entry.ts',
-  output:  {
-    file:      'resources/main.min.js',
-    name:      'main.js',
-    format:    'iife', // immediately-invoked function expression — suitable for <script> tags
-    sourcemap: !production
-  },
-  plugins: [
-    ...commonPlugins,
-    postcss(
-      {
-        extract:   true,
-        minimize:  production,
-        sourceMap: !production,
-        plugins:   postcssPlugins
-      }) // Extract css from js
-  ]
-};
+export default Flutter.getRollupConfig();
--- tailwind.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  content: ['src/**/*.{php,phtml}', 'assets/**/*.{js,ts}', 'assets/**/*.{scss,css}'],
-  theme:   {
-    extend: {}
-  },
-  plugins: []
-};
