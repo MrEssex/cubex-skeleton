ddf21beba24a339f53f36e3d1399a2940959e497
Kyle Essex <kylegavinessex@hotmail.com>
2022-11-08 18:01:36 +0000

    Update description

--- assets/entry.ts
+++ assets/entry.ts
@@ -1,4 +1,6 @@
-// Sass Main File
+/**
+ * This file is used by rollup to pull in typescript and scss files. This will be split when compiled
+ */
 import './scss/application.scss';
 
 import './ts/calculation';
