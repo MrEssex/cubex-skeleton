f4debd7392b4e5bd8753951e2e687f4b17fa4332
Kyle Essex <kylegavinessex@hotmail.com>
2023-06-15 09:31:02 +0100

    Remove seo manager (use meta)

--- src/Context/AppContext.php
+++ src/Context/AppContext.php
@@ -14,8 +14,6 @@ class AppContext extends Context implements Translatable
   use TranslatableTrait;
   use GetTranslatorTrait;
 
-  protected ?SeoManager $_seo = null;
-
   /**
    * @param string   $path
    * @param string[] $query
@@ -25,16 +23,6 @@ class AppContext extends Context implements Translatable
   public function linkBuilder(string $path = '', array $query = []): LinkBuilder
   {
     return LinkBuilder::fromRequest($this->request(), $path, $query)->setSubDomain('www');
-  }
-
-  public function seo(): SeoManager
-  {
-    if($this->_seo === null)
-    {
-      $this->_seo = SeoManager::withContext($this);
-    }
-
-    return $this->_seo;
   }
 
   public function getSiteName(): string
