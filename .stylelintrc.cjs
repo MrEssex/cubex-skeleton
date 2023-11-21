bbd5978c274a8fe813e7ec53a720b5c18650aeb0
Kyle Essex <kylegavinessex@hotmail.com>
2023-11-21 09:40:09 +0000

    Add test tsconfig for mocha

--- .stylelintrc.cjs
+++ .stylelintrc.cjs
@@ -1,14 +1,14 @@
-const {interaction} = require('stylelint-config-clean-order/src/groups/interaction');
-const {positioning} = require('stylelint-config-clean-order/src/groups/positioning');
-const {layout} = require('stylelint-config-clean-order/src/groups/layout');
-const {boxModel} = require('stylelint-config-clean-order/src/groups/box-model');
-const {typography} = require('stylelint-config-clean-order/src/groups/typography');
-const {apperance} = require('stylelint-config-clean-order/src/groups/apperance');
-const {svgPresentation} = require('stylelint-config-clean-order/src/groups/svg-presentation');
-const {transition} = require('stylelint-config-clean-order/src/groups/transition');
+const { interaction } = require("stylelint-config-clean-order/src/groups/interaction");
+const { positioning } = require("stylelint-config-clean-order/src/groups/positioning");
+const { layout } = require("stylelint-config-clean-order/src/groups/layout");
+const { boxModel } = require("stylelint-config-clean-order/src/groups/box-model");
+const { typography } = require("stylelint-config-clean-order/src/groups/typography");
+const { apperance } = require("stylelint-config-clean-order/src/groups/apperance");
+const { svgPresentation } = require("stylelint-config-clean-order/src/groups/svg-presentation");
+const { transition } = require("stylelint-config-clean-order/src/groups/transition");
 
 const propertyGroups = [
-  ['all'],
+  ["all"],
   interaction,
   positioning,
   layout,
@@ -21,37 +21,37 @@ const propertyGroups = [
 
 const propertiesOrder = propertyGroups.map((properties) => ({
   noEmptyLineBetween: true,
-  emptyLineBefore: 'never', // Don't add empty lines between order groups.
+  emptyLineBefore: "never", // Don't add empty lines between order groups.
   properties
 }));
 
 module.exports = {
-  'extends': [
-    'stylelint-config-standard',
-    'stylelint-config-sass-guidelines',
-    'stylelint-config-clean-order'
+  "extends": [
+    "stylelint-config-standard",
+    "stylelint-config-sass-guidelines",
+    "stylelint-config-clean-order"
   ],
-  'rules': {
-    'max-nesting-depth': 3,
-    'at-rule-empty-line-before': [
-      'always',
+  "rules": {
+    "max-nesting-depth": 3,
+    "at-rule-empty-line-before": [
+      "always",
       {
         ignore: [
-          'first-nested',
-          'blockless-after-same-name-blockless',
-          'after-comment',
-          "else"
-        ]
+          "first-nested",
+          "blockless-after-same-name-blockless",
+          "after-comment"
+        ],
+        ignoreAtRules: ["else"]
       }
     ],
-    'order/properties-order': [propertiesOrder, {severity: 'warning', unspecified: 'bottomAlphabetical'}],
-    'scss/at-rule-no-unknown': null,
-    'selector-no-qualifying-type': null,
-    'media-query-no-invalid': null,
+    "order/properties-order": [propertiesOrder, { severity: "warning", unspecified: "bottomAlphabetical" }],
+    "scss/at-rule-no-unknown": null,
+    "selector-no-qualifying-type": null,
+    "media-query-no-invalid": null,
     "block-closing-brace-newline-after": [
       "always", {
-        "ignoreAtRules": [ "if", "else" ]
+        ignoreAtRules: ["if", "else"]
       }
-    ],
+    ]
   }
 };
